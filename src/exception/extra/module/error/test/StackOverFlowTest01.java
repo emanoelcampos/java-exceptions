123d325dd73afbf053d8e0eaa7b3051175f059fc
emanoelcampos <emanoel.cv@gmail.com>
2023-06-25 23:34:14 -0300

    refactor packages

--- src/exception/extra/module/error/test/StackOverFlowTest01.java
+++ src/exception/extra/module/error/test/StackOverFlowTest01.java
@@ -1,4 +1,7 @@
-package exception.extra.module.test;
+package exception.extra.module.error.test;
+
+//Error é algo que acontece com a JVM e não é possível recuperar em tempo de execução
+//A solução é consertar seu código
 
 public class StackOverFlowTest01 {
 
